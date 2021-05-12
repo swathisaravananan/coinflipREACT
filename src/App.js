fed711a7c9b7a9a1593b6df72acf8d0dcac9eebb
swathisaravananan <66671402+swathisaravananan@users.noreply.github.com>
2021-05-12 11:34:59 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import FlipCoin from './FlipCoin'
+ 
+const App=()=> {
+  return (
+    <div className="App">
+      <FlipCoin />
+    </div>
+  );
+}
+ 
+export default App;
---
+++ src/Coin.js
@@ -0,0 +1,16 @@
+import React,{ Component } from 'react'
+ 
+class Coin extends Component{
+  render(){
+    return(
+      <div class='Coin'>
+        <img
+          style={{ width:'200px', height:'200px' }}
+          src={this.props.info.imgSrc}
+        />
+      </div>
+    )
+  }
+}
+ 
+export default Coin
